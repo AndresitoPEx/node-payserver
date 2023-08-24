05743febfa90063a5643c26cc68af041acb5c777
Jorge Romo <andresrt952@gmail.com>
2023-08-24 10:53:00 -0500

    implementando IPN

--- index.js
+++ index.js
@@ -1,66 +1,87 @@
-const express = require('express');
-const cors = require('cors');
-const { createFormToken } = require('./createPayment');
-const hmacSHA256 = require('crypto-js/hmac-sha256');
-const Hex = require('crypto-js/enc-hex');
-const app = express();
-const port = 2000;
+  const express = require('express');
+  const cors = require('cors');
+  const { createFormToken } = require('./createPayment');
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+  const hmacSHA256 = require('crypto-js/hmac-sha256');
+  const Hex = require('crypto-js/enc-hex');
+  const app = express();
+  const port = 2000;
 
-// Body parser
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(cors());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
-app.get('/', (req, res) => {
-  res.send('Funciona'); 
-});
+  // Body parser
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-/**
- * Generates a payment token for the given configuration
- */
-app.post('/createPayment', async (req, res) => {
-  const paymentConf = req.body.paymentConf;
+  app.get('/', (req, res) => {
+    res.send('Funciona'); 
+  });
 
-  try {
-    console.log('Received paymentConf:', paymentConf);
-    const formToken = await createFormToken(paymentConf);
-    console.log('Generar formToken:', formToken);
-    res.send(formToken);
-  } catch (error) {
-    console.error('Error formToken', error);
-    res.status(500).send(error.data);
+  /**
+   * Generates a payment token for the given configuration
+   */
+  app.post('/createPayment', async (req, res) => {
+    const paymentConf = req.body.paymentConf;
+
+    try {
+      console.log('Received paymentConf:', paymentConf);
+      const formToken = await createFormToken(paymentConf);
+      console.log('Generar formToken:', formToken);
+      res.send(formToken);
+    } catch (error) {
+      console.error('Error formToken', error);
+      res.status(500).send(error.data);
+    }
+  });
+
+  /**
+   * Validates the given payment data (hash)
+   */
+  app.post('/validatePayment', (req, res) => {
+    const answer = req.body.clientAnswer;
+    const hash = req.body.hash;
+    const answerHash = Hex.stringify(
+      hmacSHA256(JSON.stringify(answer), '40hE0bHKAQPGZZyyM74W0XnY8SWqIynq6xdrLG6GgTbCS')
+    );
+
+    console.log('Received clientAnswer:', answer);
+    console.log('Received hash:', hash);
+    console.log('Calculated answerHash:', answerHash);
+
+    if (hash === answerHash) {
+      console.log('Payment is valid');
+      res.status(200).send('Valid payment');
+    } else {
+      console.log('Payment hash mismatch');
+      res.status(500).send('Payment hash mismatch');
+    }
+  });   
+
+  // IPN Endpoint
+app.post('/ipn', (req, res) => {
+  // Verify the hash
+  const receivedHash = req.headers['hash'];
+  const computedHash = Hex.stringify(hmacSHA256(JSON.stringify(req.body), '40hE0bHKAQPGZZyyM74W0XnY8SWqIynq6xdrLG6GgTbCS'));
+
+  if (receivedHash !== computedHash) {
+    return res.status(400).send('Hash mismatch');
   }
+
+  // Process the notification
+  const transactionStatus = req.body.status;
+  // Update the transaction status in your database
+
+  // Respond to the notification
+  res.status(200).send('OK');
 });
 
-/**
- * Validates the given payment data (hash)
- */
-app.post('/validatePayment', (req, res) => {
-  const answer = req.body.clientAnswer;
-  const hash = req.body.hash;
-  const answerHash = Hex.stringify(
-    hmacSHA256(JSON.stringify(answer), '40hE0bHKAQPGZZyyM74W0XnY8SWqIynq6xdrLG6GgTbCS')
-  );
 
-  console.log('Received clientAnswer:', answer);
-  console.log('Received hash:', hash);
-  console.log('Calculated answerHash:', answerHash);
 
-  if (hash === answerHash) {
-    console.log('Payment is valid');
-    res.status(200).send('Valid payment');
-  } else {
-    console.log('Payment hash mismatch');
-    res.status(500).send('Payment hash mismatch');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
