6b0daa207b8ea82d7bf0aaadc54e9fcf308d4c7d
Jorge Romo <andresrt952@gmail.com>
2023-08-28 13:27:30 -0500

    prubeas de produccion

--- createPayment.js
+++ createPayment.js
@@ -5,7 +5,7 @@ exports.createFormToken = async paymentConf => {
   const username = '14245093'
 
   // format: testprivatekey_XXXXXXX
-  const password = 'testpassword_6JpDLl2tGvCShpS1tJWXHV6sVPJpLiCGOUAzvkJtl2HZk'
+  const password = 'prodpassword_IcgOK9mgP3DPQNjzozrXIOSftgpifFCWugakZAZrib9fE'
 
   // format: api.my.psp.domain.name without https
   const endpoint = 'api.micuentaweb.pe'
